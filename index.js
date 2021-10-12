dc133b72681525a0b9cacb90044d80a80336e390
Geoffrey Odari <49524742+geoffreyodari@users.noreply.github.com>
2021-10-12 15:25:35 +0000

    Added comments

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-let beast = "great blue heron"
-let dish ="garlic naan"
+//let beast = "great blue heron"
+//let dish ="garlic naan"
 
-const feast = (beast,meal)=>beast[0]===dish[0]&&beast[beast.length-1]===dish[dish.length-1]
 
-feast(beast,dish)
+
+//if the first name of beast and dish and last name of beast and dish are same the function returns true  and false if the condititons are not met
+const feast = (beast,meal)=>beast[0]===dish[0]&&beast[beast.length-1]===dish[dish.length-1];
+
+feast("great blue heron","garlic naan");
